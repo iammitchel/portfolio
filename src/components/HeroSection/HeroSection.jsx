0d9d90bf38fe2bda1c2d698835aee5e27e5d9cb8
iamogbumitchel <mitchelogbu47@gmail.com>
2023-05-21 17:49:26 +0100

    updated

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -38,10 +38,45 @@ const HeroSection = () => {
               </h2>
 
               <p>
-                Ugochukwu is a frontend developer who loves to explore
+                {/* Ugochukwu is a frontend developer who loves to explore
                 opportunities that make room for the attainment of skills,
                 nurturing of potentials and capabilities in the utilization of
-                modern technology.
+                modern technology. */}
+                Ugochukwu is a highly skilled and experienced front-end
+                developer with a passion for creating visually appealing and
+                user-friendly web interfaces. With 3 years of industry
+                experience, I have developed a deep understanding of front-end
+                technologies and best practices. <br />
+                <br />
+                Throughout my career, I have demonstrated expertise in HTML,
+                CSS, and JavaScript, the core building blocks of front-end
+                development. I have a strong command of HTML5 and CSS3,
+                utilizing their knowledge to create semantic and accessible web
+                pages. I am well-versed in JavaScript framework such as React,
+                enabling me to develop dynamic and interactive user interfaces.{" "}
+                <br />
+                <br />
+                I have a keen eye for design and understands the importance of
+                user experience (UX) in web development. I collaborate closely
+                with UX/UI designers, translating their mockups and wireframes
+                into pixel-perfect and responsive web designs. I have experience
+                in responsive design techniques, ensuring that websites and
+                applications look and function flawlessly across different
+                devices and screen sizes. <br />
+                <br />
+                In addition to my technical skills, I possess strong
+                problem-solving abilities and an analytical mindset. I am adept
+                at debugging and troubleshooting, able to identify and resolve
+                front-end issues efficiently. I stay up-to-date with the latest
+                industry trends, attending conferences and participating in
+                online communities to expand their knowledge and skills. <br />
+                <br />
+                Overall, I am a dedicated front-end developer with a strong
+                foundation in front-end technologies, a keen eye for design, and
+                a commitment to delivering high-quality user experiences. My
+                technical proficiency, problem-solving abilities, and
+                collaborative nature make me a valuable asset to any
+                development team.
               </p>
             </div>
           </Col>
